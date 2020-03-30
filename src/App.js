8ccd71ffab08195b34556a9d4f41b0b26541878c
Heo Seonjo <hizzang@gmail.com>
2020-03-31 01:36:44 +0900

    #1 Clear

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { useState, useCallback } from 'react';
-import axios from 'axios';
-import NewsList from './NewsLists';
-import Categories from './Components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPages';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category => setCategory(category), []);
-
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get('http://newsapi.org/v2/top-headlines?country=kr&apiKey=64ff34566c1e49398b0cf686cc89628f');
-
-      setData(response.data);
-    
-    } catch(e){
-      console.log(e);
-    }
-    
-  };
-
+  
   return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-
+    <Route path="/:category?" component={NewsPage}></Route>
   )
 }
 
